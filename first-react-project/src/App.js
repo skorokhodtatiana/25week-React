73ea0dc938ec2310b162c0e11bf3a79cf6b86d54
skorokhodtatiana <tanjakharza@mail.ru>
2021-10-27 17:46:14 +0300

    created react structure project, added chart pie

---
+++ first-react-project/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import PieChart from './components/PieChart';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     <PieChart/>
+    </div>
+  );
+}
+
+export default App;
